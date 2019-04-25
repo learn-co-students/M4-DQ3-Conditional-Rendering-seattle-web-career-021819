86a60fa096bf1909f269aecfef8b3cdbd0dce7b2
isalevine <isaacdlevine@gmail.com>
2019-04-25 09:20:36 -0700

    Done.

--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -4,20 +4,49 @@ import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {
+      currentTab: null
+    }
+    this.selectTab = this.selectTab.bind(this)
+  }
+
+  selectTab(tabId) {
+    console.log("selectTab called for tabId: ", tabId)
+    this.setState({currentTab: tabId})
+  }
 
   render() {
 
     /*
 
-    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
+    Replace the code below! Depending on what menu item is
+    selected in the menu, I should render either a Profile,
+    Photos, Cocktails, or Pokemon component.Think of a way
+    to track which menu item was selected. Which component
+    should have state? Which component should have methods
+    to control state? Where should these methods be called?
 
     */
 
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
+    let detailsToDisplay = null;
+    if (this.state.currentTab === "profile") {
+      detailsToDisplay = <Profile />
+    }
+    else if (this.state.currentTab === "photo") {
+      detailsToDisplay = <Photos />
+    }
+    else if (this.state.currentTab === "cocktail") {
+      detailsToDisplay = <Cocktails />
+    }
+    else if (this.state.currentTab === "pokemon") {
+      detailsToDisplay = <Pokemon />
+    }
 
     return (
       <div>
-        <MenuBar />
+        <MenuBar selectTab={this.selectTab}/>
         {detailsToDisplay}
       </div>
     )
